258b4c7ee0ffcc14805ac0559cef7a17ead62a8c
Brandon Bloch <brandon@bloch.ca>
2016-02-25 09:19:04 -0500

    Consolidated CSS files
    Moved all CSS files from the root directory to /library/css and updated
    references by Core functions

--- library/php/Core.php
+++ library/php/Core.php
@@ -6,7 +6,7 @@ class Core {
 	 *
 	 */
 	public static function includeCore() {
-		echo '<link rel="stylesheet" href="style.css">' . PHP_EOL;
+		echo '<link rel="stylesheet" href="library/css/style.css">' . PHP_EOL;
 		echo '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">' . PHP_EOL;
 		include "actions/adminbar.php";
 	}
